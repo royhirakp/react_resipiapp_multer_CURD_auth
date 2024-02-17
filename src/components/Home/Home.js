36dd732b34f74a52c0a3e0d91a4c76ede07b038b
Hirak Roy <royhirakp@gmail.com>
2024-02-17 11:58:25 +0530

    error

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,99 +1,96 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import Header from '../Card/Header'
-import RecipiCard from '../Card/RecipiCard'
-import "./Home.css"
-import Loader from '../Card/Loder'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Header from "../Card/Header";
+import RecipiCard from "../Card/RecipiCard";
+import "./Home.css";
+import Loader from "../Card/Loder";
 import { useNavigate } from "react-router-dom";
-import LocalPizzaIcon from '@mui/icons-material/LocalPizza';
+import LocalPizzaIcon from "@mui/icons-material/LocalPizza";
 const Home = () => {
-  const [data , setData]=useState([])
-  const [searchData, setSearchData] = useState('')
+  const [data, setData] = useState([]);
+  const [searchData, setSearchData] = useState("");
   const navgate = useNavigate();
-  let [loder, setLoader] = useState(false)
+  let [loder, setLoader] = useState(false);
 
- 
-
-  useEffect(()=>{
-    if(!localStorage.getItem('logToken')){
-        navgate('/')
+  useEffect(() => {
+    if (!localStorage.getItem("logToken")) {
+      navgate("/");
     }
-},[])
+  }, [navgate]);
 
+  function search() {
+    let result = [];
+    // console.log(searchData)
+    // console.log(data)
 
-function search(){
-  let result = [];
-  // console.log(searchData)
-  // console.log(data)
-
-  for(let i =0; i<data.length; i++){
-    if(data[i].title.includes(searchData)){
-      result.push(data[i])
-    }
-  }
-  // console.log(result,"<<<result");
-  setData(result)
- 
-}
-
-
-
-useEffect(()=>{
-  async function datafunction () {
-      setLoader(true)
-      try {
-          const config = {
-              headers: {
-                  authorization : localStorage.getItem('logToken')                      
-              }
-          }
-          const res = await axios.get ('https://prt-25feb-nodejs.onrender.com/reci',config)
-              setData(res.data.recipi) 
-              console.log(res.data.recipi)       
-      } catch (error) {
-          console.log(error)
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].title.includes(searchData)) {
+        result.push(data[i]);
       }
-      setLoader(false)
+    }
+    // console.log(result,"<<<result");
+    setData(result);
   }
-  datafunction()
-},[])
+
+  useEffect(() => {
+    async function datafunction() {
+      setLoader(true);
+      try {
+        const config = {
+          headers: {
+            authorization: localStorage.getItem("logToken"),
+          },
+        };
+        const res = await axios.get(
+          "https://prt-25feb-nodejs.onrender.com/reci",
+          config
+        );
+        setData(res.data.recipi);
+        console.log(res.data.recipi);
+      } catch (error) {
+        console.log(error);
+      }
+      setLoader(false);
+    }
+    datafunction();
+  }, []);
 
   return (
     <div>
-      <Header/>
-      
-      <div className='searchContainer'>
+      <Header />
+
+      <div className="searchContainer">
         <div>
-        <input type="text"
-        onChange={(e)=>setSearchData(e.target.value)}
-         placeholder='search here...' />
-        <button 
-        onClick={search}
-        className="searchbutton" >search</button>
+          <input
+            type="text"
+            onChange={(e) => setSearchData(e.target.value)}
+            placeholder="search here..."
+          />
+          <button onClick={search} className="searchbutton">
+            search
+          </button>
         </div>
       </div>
-      <br/>
-        <div className='addRecipiContainer'>
-          <button onClick={()=>  navgate('../create')}>
-            <LocalPizzaIcon/>add
-            </button>
-            </div>
-        <h1>Allrecipites</h1>
-      <div className='allrecipies'>
-        {
-          data.map((item,i)=>{
-            return (
-              <div key={i*0.0022}>
-                <RecipiCard data={item} />
-              </div>
-              
-            )
-          })
-        }
+      <br />
+      <div className="addRecipiContainer">
+        <button onClick={() => navgate("../create")}>
+          <LocalPizzaIcon />
+          add
+        </button>
       </div>
-      {loder?<Loader/>:<></>}
+      <h1>Allrecipites</h1>
+      <div className="allrecipies">
+        {data.map((item, i) => {
+          return (
+            <div key={i * 0.0022}>
+              <RecipiCard data={item} />
+            </div>
+          );
+        })}
+      </div>
+      {loder ? <Loader /> : <></>}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
